24fd675c6d2afb4edd213e99fe909a377a1e51d1
arturol76 <arturo.lotito@gmail.com>
2019-10-15 08:18:39 +0200

    check num of params

--- build.sh
+++ build.sh
@@ -1,16 +1,35 @@
-echo ------------------------------------------------------
-echo USAGE:
-echo ./build.sh docker_ip image_name zm_version disable_ssh
-echo    -docker_ip: ip of docker host [127.0.0.1 or others]
-echo    -zm_version: 1.30, 1.32, master
-echo
-echo example:
-echo    ./build.sh 192.168.2.96 master
-echo ------------------------------------------------------
-echo
+#!/bin/bash
+show_help()
+{
+    echo ------------------------------------------------------
+    echo USAGE:
+    echo ./build.sh docker_host image_name zm_version
+    echo    -docker_host: ip of docker host [127.0.0.1 or others]
+    echo    -zm_version: 1.30, 1.32, master
+    echo
+    echo example:
+    echo    ./build.sh 192.168.2.96 master
+    echo ------------------------------------------------------
+    echo
+}
+
+num_of_params=2
+docker_host=$1
+container_name=$2
+
+#checks number of parameters
+if [ "$#" -ne $num_of_params ]; then
+    echo "Illegal number of parameters."
+    echo
+    show_help
+    exit 1
+fi
+
+#-------chnge to your needs-------------------------
 default_repo="arturol76"
 default_image="zoneminder"
 default_tag="latest"
+#---------------------------------------------------
 
 echo
 
@@ -23,8 +42,7 @@ read -p "which tag? [$default_tag]: " tag
 tag=${tag:-$default_tag}
 
 echo building image "$repo/$image:$tag"...
-#docker -H $1 build --no-cache -t $repo/$image:$tag --build-arg ZM_VERS=$2 .
-docker -H $1 build --no-cache -t $repo/$image:$tag --build-arg ZM_VERS=$2 .
+docker -H $docker_host build --no-cache -t $repo/$image:$tag --build-arg ZM_VERS=$2 .
 
 read -p "Do you want to push image to docker repository? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -32,15 +50,16 @@ then
     echo
 
     echo pushing to $repo/$image:$tag...
-    docker -H $1 push $repo/$image:$tag
+    docker -H $docker_host push $repo/$image:$tag
 
     read -p "Do you want to tag it also with 'latest'? " -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then 
         echo pushing to $repo/$image:latest...
-        docker -H $1 push $repo/$image:latest
+        docker -H $docker_host push $repo/$image:latest
     else echo; fi
   
 else
     echo
 fi
 
+exit 0
